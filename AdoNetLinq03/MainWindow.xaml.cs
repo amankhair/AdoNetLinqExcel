1e9e250d4a618873acd8c7f69c78c34db58f0818
КаирбаевА <@403_5_>
2018-01-23 18:03:44 +0600

    Add project files.

---
+++ AdoNetLinq03/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using AdoNetLinq03.Tools;
+using System.Data;
+
+namespace AdoNetLinq03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelPackage package = new ExcelPackage();
+            string pathForSaving = "\\Template";
+            FileInfo template = new FileInfo("myTemplate.xlsx");
+            
+            using(package = new ExcelPackage(template, true))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+
+                Helper helper = new Helper();
+                DataSet data = helper.GetData();
+                int row = 3;
+
+                List<Area> areas = new List<Area>();
+
+                foreach(DataRow dataRow in data.Tables["Area"].Rows)
+                {
+                    Area area = new Area();
+                    area.AreaId = Int32.Parse(dataRow["AreaId"].ToString());
+                    area.Name = dataRow["Name"].ToString();
+                    area.FullName = dataRow["FullName"].ToString();
+                    area.PavilionId = Int32.Parse(dataRow["PavilionId"].ToString());
+
+                    areas.Add(area);
+                }
+
+                List<Pavilion> pavilions = new List<Pavilion>();
+                foreach (DataRow dataRow in data.Tables["dic_Pavilion"].Rows)
+                {
+                    Pavilion pav = new Pavilion();
+                    pav.PavilionId = Int32.Parse(dataRow["PavilionId"].ToString());
+                    pav.Name = dataRow["Name"].ToString();
+                    pavilions.Add(pav);
+                }
+
+
+                foreach (DataRow dataRow in data.Tables["Area"].Rows)
+                {
+                    worksheet.Cells[row, 2].Value = dataRow["Name"];
+                    worksheet.Cells[row, 3].Value = dataRow["FullName"];
+                    row++;
+                }
+
+                var query = areas.Join(pavilions,
+                    a => a.PavilionId,
+                    p => p.PavilionId,
+                    (a, p) => new
+                    {
+                        a.Name,
+                        a.FullName,
+                        PavilionName = p.Name
+                    });
+
+                foreach (var dataRow in query)
+                {
+                    worksheet.Cells[row, 2].Value = dataRow.Name;
+                    worksheet.Cells[row, 3].Value = dataRow.FullName;
+                    worksheet.Cells[row, 7].Value = dataRow.PavilionName;
+
+                    if (dataRow.PavilionName == "not determined")
+                        Helper.SetColor(worksheet, "#AB0909", row, 2, row, 7);
+
+                    row++;
+
+
+                }
+
+                
+
+                package.SaveAs(new System.IO.FileInfo("demoOut.xlsx"));
+            } 
+        }
+    }
+}
---
+++ AdoNetLinq03/Tools/Helper.cs
@@ -0,0 +1,85 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetLinq03.Tools
+{
+    class Helper
+    {
+        public DataSet GetData()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM [CRCMS_new].[dbo].[Area];" +
+                                    "SELECT* FROM[CRCMS_new].[dbo].[dic_Group];" +
+                                    "SELECT* FROM[CRCMS_new].[dbo].[dic_Pavilion];";
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                da.TableMappings.Add("Table", "Area");
+                da.TableMappings.Add("Table1", "dic_Group");
+                da.TableMappings.Add("Table2", "dic_Pavilion");
+
+                da.Fill(ds);
+                con.Dispose();
+                return ds;
+            }
+        }
+
+        private void SetColor(ExcelWorksheet worksheet, string color, int FRow, int FCol, int ToRow, int ToCol)
+        {
+            using (ExcelRange rng = worksheet.Cells[FRow, FCol, ToRow, ToCol])
+            {
+                if (color != null)
+                {
+                    System.Drawing.Color colContractHex =
+                        ColorTranslator.FromHtml(color);
+
+                    rng.Style.Fill.PatternType =
+                        ExcelFillStyle.Solid;
+
+                    rng.Style.Fill.BackgroundColor
+                        .SetColor(colContractHex);
+                }
+
+                //rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                //rng.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                //rng.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                //rng.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                //rng.Style.WrapText = true;
+            }
+        }
+
+    }
+
+    
+    public class Area
+    {
+        public int AreaId { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int PavilionId { get; set; }
+    }
+
+    public class Pavilion
+    {
+        public int PavilionId { get; set; }
+        public string Name { get; set; }
+    }
+}
